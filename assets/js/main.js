e999906eb71a5a0c656011fe0b607f11e2d36310
DSlundro <dragan.savic95@outlook.it>
2022-03-24 16:59:06 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,18 +14,18 @@ const prezzo_minorenni = prezzo_biglietto - (prezzo_biglietto * 0.2);
 // 40% sconto per gli over 65
 const prezzo_over_65 = prezzo_biglietto - (prezzo_biglietto * 0.4);
 
-// prezzo
+// prezzo con due decimali per indicare i centesimi sul prezzo
 if (età < 18 && età > 0) {
-    console.log(prezzo_biglietto);
-    console.log(prezzo_minorenni + ' € prezzo scontato per i minorenni');
+    console.log(prezzo_biglietto.toFixed(2) + ' €');
+    console.log(prezzo_minorenni.toFixed(2) + ' € prezzo scontato per i minorenni');
 } else if (età > 65) {
-    console.log(prezzo_biglietto);
-    console.log(prezzo_over_65 + ' € prezzo scontato per gli over 65');
+    console.log(prezzo_biglietto.toFixed(2) + ' €');
+    console.log(prezzo_over_65.toFixed(2) + ' € prezzo scontato per gli over 65');
 } else {
-    console.log(prezzo_biglietto);
+    console.log(prezzo_biglietto.toFixed(2) + ' €');
 }
 
-// prezzo con due decimali per indicare i centesimi sul prezzo
+
 
 
 
